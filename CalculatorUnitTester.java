ea5324a42c85f0ec67cccc37559a437fe5abe7b2
Ivy Chang <ivycwq@icloud.com>
2017-03-23 22:53:21 -0700

    adding test cases

--- CalculatorUnitTester.java
+++ CalculatorUnitTester.java
@@ -1,6 +1,8 @@
 import java.lang.annotation.*;
 
-@interface Init {}
+@Retention(RetentionPolicy.RUNTIME)
+
+@interface Initialize {}
 
 @interface Before {}
 
@@ -14,30 +16,30 @@ public class CalculatorUnitTester {
   private Calculator c;
   private Double expected, result;
 
-  @Init()
+  @Initialize
   public void initialize() {
     c = new Calculator();
     System.out.println("Initializing...");
   }
 
-  @Before()
+  @Before
   public void setup() {
     expected = result = 0.0;
     System.out.println("Setting up the test...");
   }
 
-  @After()
+  @After
   public void finish() {
     System.out.println("Finishing up the test...");
   }
 
-  @Cleanup()
+  @Cleanup
   public void cleanup() {
     c = null;
     System.out.println("Cleaning up after all tests...");
   }
 
-  @Test()
+  @Test
   public void testAdd() {
     expected = 11.0;
     result = c.add(5.0, 6.0);
@@ -47,7 +49,7 @@ public class CalculatorUnitTester {
       System.out.println("Test Add failed."); 
   }
 
-  @Test()
+  @Test
   public void testSubtract() {
     expected = -1.0;
     result = c.subtract(5.0, 6.0);
@@ -57,7 +59,7 @@ public class CalculatorUnitTester {
       System.out.println("Test Subtract failed."); 
   }
 
-  @Test()
+  @Test
   public void testMultiply() {
     expected = 30.0;
     result = c.multiply(5.0, 6.0);
@@ -67,7 +69,7 @@ public class CalculatorUnitTester {
       System.out.println("Test Multiply failed."); 
   }
 
-  @Test()
+  @Test
   public void testDivide() {
     expected = 0.825;
     result = c.divide(5.0, 6.0);
@@ -77,7 +79,7 @@ public class CalculatorUnitTester {
       System.out.println("Test Divide failed."); 
   }
 
-  @Test()
+  @Test
   public void testDivideException() {
     try {
       c.divide(5.0, 0.0);
--- UnitTestDriver.java
+++ UnitTestDriver.java
@@ -1,3 +1,57 @@
+import java.lang.reflect.*;
+
 public class UnitTestDriver {
-  
+
+  public static void main(String[] args) {
+    CalculatorUnitTester cut = new CalculatorUnitTester();
+    Class<?> type = cut.getClass();
+
+    for (Method m : type.getDeclaredMethods()) {
+      if (m.isAnnotationPresent(Initialize.class)) {
+        try {
+          m.invoke(cut);
+        } catch(IllegalAccessException e) {
+          System.err.println(e);
+        } catch(InvocationTargetException o) {
+          System.err.println(o);
+        }  
+      }
+    }
+
+    Method before = null;
+    Method after = null;
+
+    try {
+      before = type.getMethod("setup"); 
+      after = type.getMethod("finish");
+    } catch (NoSuchMethodException e) {
+      System.err.println(e);
+    }
+
+    for (Method m : type.getDeclaredMethods()) {
+      if (m.isAnnotationPresent(Test.class)) {
+        try {
+          before.invoke(cut);
+          m.invoke(cut);
+          after.invoke(cut);
+        } catch(IllegalAccessException e) {
+          System.err.println(e);
+        } catch(InvocationTargetException o) {
+          System.err.println(o);
+        }  
+      }
+    }
+
+    for (Method m : type.getDeclaredMethods()) {
+      if (m.isAnnotationPresent(Cleanup.class)) {
+        try {
+          m.invoke(cut);
+        } catch(IllegalAccessException e) {
+          System.err.println(e);
+        } catch(InvocationTargetException o) {
+          System.err.println(o);
+        }  
+      }
+    }    
+  }
 }
