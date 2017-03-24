4710e266e6e38601d16c1fffbee4326a94b9ca8e
Ivy Chang <ivycwq@icloud.com>
2017-03-23 18:23:51 -0700

    making them instance methods

--- Calculator.java
+++ Calculator.java
@@ -1,18 +1,18 @@
 public class Calculator {
 
-  public static double add(double a, double b) {
+  public double add(double a, double b) {
     return a + b;
   }
 
-  public static double subtract(double a, double b) {
+  public double subtract(double a, double b) {
     return a - b;
   }
 
-  public static double multiply(double a, double b) {
+  public double multiply(double a, double b) {
     return a * b;
   }
 
-  public static double divide(double a, double b) {
+  public double divide(double a, double b) {
     if (b == 0)
       throw new IllegalArgumentException("Devisor cannot be zero!");
     return a / b;
